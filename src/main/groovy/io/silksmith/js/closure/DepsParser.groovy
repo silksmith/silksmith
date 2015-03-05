5782cc02603a06a20728e2926aa66435d1034978
Thomas Meyer <thomas@meyertee.com>
2015-03-05 14:05:56 +0100

    Add support for Closure modules in DepsParser & DepsJSHandler

--- src/main/groovy/io/silksmith/js/closure/DepsParser.groovy
+++ src/main/groovy/io/silksmith/js/closure/DepsParser.groovy
@@ -2,16 +2,10 @@ package io.silksmith.js.closure
 
 class DepsParser {
 
-
-
-	//XXX:not super correct since goog.provide("stuf') would be possible
-	def providePattern = /goog\.provide\(["']([\w\.]*)["']\);?/
-	def requirePattern = /goog\.require\(["']([\w\.]*)["']\);?/
-
-	//	def providePattern = ~/goog\.provide\("([\w\.]*)"\);*/
-	//	def requirePattern = ~/goog\.require\("([\w\.]*)"\);*/
-	//	def providePattern2 = ~/goog\.provide\('([\w\.]*)'\);*/
-	//	def requirePattern2 = ~/goog\.require\('([\w\.]*)'\);*/
+	// See: https://github.com/google/closure-library/blob/master/closure/bin/build/source.py
+	def providePattern = /(?m)^\s*goog\.provide\(\s*['"](.+)['"]\s*\)/
+	def modulePattern = /(?m)^\s*goog\.module\(\s*['"](.+)['"]\s*\)/
+	def requirePattern = /(?m)^\s*(?:(?:var|let|const)\s+[a-zA-Z\_\$][a-zA-Z0-9\$\_]*\s*=\s*)?goog\.require\(\s*[\'"](.+)[\'"]\s*\)/
 
 	def symbolCollector = { m, symbol -> return symbol }
 
@@ -21,11 +15,15 @@ class DepsParser {
 			def provideMatcher = it.text =~providePattern
 			def provides = provideMatcher.collect symbolCollector
 
-
 			def requireMatcher = it.text =~requirePattern
 			def requires = requireMatcher.collect symbolCollector
 
-			new FileInfo(file:it, provides: provides.sort(), requires:requires.sort())
+			def moduleMatcher = it.text =~modulePattern
+			def modules = moduleMatcher.collect symbolCollector
+
+			provides += modules
+
+			new FileInfo(file:it, provides: provides.sort(), requires:requires.sort(), isModule:!modules.empty)
 		}).sort({it.file.path})
 	}
 }
