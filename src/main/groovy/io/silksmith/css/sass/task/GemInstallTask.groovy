0cf246656dbe2f7a12f7335a8a673253a573db39
Max Bruchmann <bruchmann@signal7.de>
2015-06-22 23:36:57 +0200

    removed jruby configuration

--- src/main/groovy/io/silksmith/css/sass/task/GemInstallTask.groovy
+++ src/main/groovy/io/silksmith/css/sass/task/GemInstallTask.groovy
@@ -5,11 +5,14 @@ import org.gradle.api.artifacts.Configuration
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
+import org.jruby.embed.LocalContextScope;
+import org.jruby.embed.LocalVariableBehavior;
+import org.jruby.embed.ScriptingContainer;
 
 class GemInstallTask extends DefaultTask {
 
 
-	Configuration jrubyConfig
+	
 
 	@OutputDirectory
 	def gemInstallDir
@@ -23,38 +26,56 @@ class GemInstallTask extends DefaultTask {
 
 	@TaskAction
 	def install() {
-
+		ScriptingContainer container = new ScriptingContainer(LocalContextScope.SINGLETHREAD, LocalVariableBehavior.PERSISTENT);
+		container.environment = ['GEM_PATH':gemInstallDir.path]
+		//container.argv = sassArgs
+		
+		def installScript = """
+require 'rubygems' 
+require 'rubygems/dependency_installer.rb' 
+inst = Gem::DependencyInstaller.new :install_dir => "$gemInstallDir" 
+"""
 
 		gemInstallDir.mkdirs()
+		
+		def installScriptResult = container.runScriptlet(installScript)
 
-		gems.each { gem ->
-			logger.info "Installing $gem"
-			project.javaexec({
-				main = "org.jruby.Main"
-				classpath = jrubyConfig
-				def jRubyArgs = [
-					"-S",
-					"gem",
-					"install",
-					"-i",
-					"$gemInstallDir"
-				]
-
-				jRubyArgs << "$gem.key"
-				if(gem.value) {
-					jRubyArgs << "$gem.value"
-				}
-				jRubyArgs <<"--no-rdoc"
-				jRubyArgs << "--no-ri"
-
-				args = jRubyArgs
-
-				environment 'PATH', "$gemInstallDir/bin"
-				environment 'GEM_PATH', gemInstallDir
-
-				println "Executing ${commandLine.join(' ')}"
-			})
+		gems.each {gem -> 
+			def gemInstall = "inst.install '$gem.key'"
+			if(gem.value) {
+				gemInstall += ", '$gem.value'"
+			}
+			container.runScriptlet(gemInstall)
 		}
+		logger.info("installScript result",installScriptResult)
+//		gems.each { gem ->
+//			logger.info "Installing $gem"
+//			project.javaexec({
+//				main = "org.jruby.Main"
+//				classpath = jrubyConfig
+//				def jRubyArgs = [
+//					"-S",
+//					"gem",
+//					"install",
+//					"-i",
+//					"$gemInstallDir"
+//				]
+//
+//				jRubyArgs << "$gem.key"
+//				if(gem.value) {
+//					jRubyArgs << "$gem.value"
+//				}
+//				jRubyArgs <<"--no-rdoc"
+//				jRubyArgs << "--no-ri"
+//
+//				args = jRubyArgs
+//
+//				environment 'PATH', "$gemInstallDir/bin"
+//				environment 'GEM_PATH', gemInstallDir
+//
+//				println "Executing ${commandLine.join(' ')}"
+//			})
+//		}
 
 
 		//		ScriptEngineManager manager = new ScriptEngineManager()
